f14c6d7d5ab50b2be8af0207766f2ba14d51c1f3
Shivam Rathod <rathodshivam05@gmail.com>
2022-05-06 20:27:39 +0530

    navbar added

---
+++ scripts/header1.js
@@ -0,0 +1,17 @@
+import header from "../components/header.js";
+
+document.getElementById("header").innerHTML = header();
+
+let shopByCat= document.getElementById("s-cat")
+shopByCat.addEventListener("mouseover", showDiv);
+
+let container = document.getElementById("s-container")
+function showDiv(e){
+    e.stopPropagation()
+   // console.log(e)
+container.removeAttribute("hidden");
+}
+document.body.addEventListener("click", ()=>{
+container.setAttribute("hidden", true)
+})
+
