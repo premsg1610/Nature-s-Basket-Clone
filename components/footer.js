0ea746eb63c18276c6853e03d21c63045b00923e
adityamuthal22 <adityamuthal.2208@gmail.com>
2022-05-06 20:02:29 +0530

    Footer Added

---
+++ components/footer.js
@@ -0,0 +1,238 @@
+let footer =() =>{
+    return `
+    <div id="Adifooter1">
+    <div id="Adisecbox1">
+        <h4>DOWNLOAD THE APP</h4>
+        <a href="https://play.google.com/store/apps/details?id=com.godrej.naturesbasketltd"><img src="https://cdn.worldvectorlogo.com/logos/get-it-on-google-play.svg" alt=""></a>
+        <a href="https://apps.apple.com/in/app/natures-basket/id1013402486"><img src="https://cdn.worldvectorlogo.com/logos/available-on-the-app-store.svg" alt=""></a>
+    </div>
+    <div id="Adisecbox2">
+        <div>
+            <ul id="Adisecrow1">
+                <li>
+                    <h4><a href="">ABOUT US</a></h4>
+                </li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Our Vision & Purpose</a></li>
+                <li><a href="">Quality standards</a></li>
+                <li><a href="">Awards</a></li>
+                <li><a href="">FAQs</a></li>
+                <li><a href="">Available Delivery Slot</a></li>
+                <li><a href="">Vender Connect</a></li>
+                <li>
+                    <h4><a href="">CAREERS</a></h4>
+                </li>
+                <li><a href="">Work with us</a></li>
+            </ul>
+
+        </div>
+        <div>
+            <ul id="Adisecrow1">
+                <li>
+                    <h4><a href="">News & Media</a></h4>
+                </li>
+                <li><a href="">Press releases</a></li>
+                <li><a href="">Newsletters</a></li>
+                <li><a href="">Events</a></li>
+                <li><a href="">Photo gallery</a></li>
+                <li><a href="">Recipes</a></li>
+                <li><a href="">NBTV</a></li>
+                <li><a href="">Our Vitual Store</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul id="Adisecrow1">
+                <li>
+                    <h4><a href="">Our Policies</a></h4>
+                </li>
+                <li><a href="">Privacy Plolicy</a></li>
+                <li><a href="">Payment policy</a></li>
+                <li><a href="">Disclaimer</a></li>
+                <li><a href="">Statutory Compliance</a></li>
+                <li><a href="">Data Protection Plolicy</a></li>
+                <li><a href="">Grievance Officer</a></li>
+                <li><a href="">Information Security Policy</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="Adinewsletter">
+            <h4>SIGN UP TO STAY UPDATED</h4>
+            <input type="text" placeholder="ENTER YOUR EMAIL ADDRESS">
+            <button id="AdiSubbutton">SUBSCIBE</button>
+           
+    </div>
+</div>
+
+<!-- 2 Footer Start -->
+<div id="Adicolpay">
+    <div id="Adirow1pay"><span>Payment Method </span> <img src="https://www.naturesbasket.co.in/Images/icons-card-payments-mode.png" alt=""></div>
+    <div id="Adirow2pay">
+            <ul>
+                <li>Keep in Touch</li>
+                <li><a href="https://www.facebook.com/NaturesBasket"><i class="fa-brands fa-facebook-f"></i></a></li>
+                <li><a href="https://twitter.com/NaturesBasket"><i class="fa-brands fa-twitter"></i></a></li>
+                <li><a href="https://www.youtube.com/user/GodrejNaturesBasket"><i class="fa-brands fa-youtube"></i></a></li>
+                <li><a href="https://www.instagram.com/NaturesBasket/"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="https://in.pinterest.com/naturesbasket/"><i class="fa-brands fa-pinterest"></i></a></li>
+            </ul>
+    </div>
+</div>
+<!-- 3 Footer start -->
+<div id="Adifooter3">
+    <div>
+        <ul id="Adisecrow1">
+            <li>
+                <h4><a href="">FRESH & FAST</a></h4>
+            </li>
+            <li>
+                <h4><a href="">GIFT HAMPERS</a></h4>
+            </li>
+            <li><a href="">Birthday & Anniversary Gift Hampers</a></li>
+            <li><a href="">World Cuisine Gift Hampers</a></li>
+            <li><a href="">Organic & Health Gift Hampers</a></li>
+            <li><a href="">Coffee & Tea Gift Hampers</a></li>
+            <li><a href="">Get Well Soon Gift Hampers</a></li>
+            <li><a href="">Cheese & Meat Platters</a></li>
+            <li>
+                <h4><a href="">FRUITS & VEGETABLES</a></h4>
+            </li>
+            <li><a href="">Vegetables</a></li>
+            <li><a href=""> Exotic Vegetables</a></li>
+            <li><a href="">Fruits</a></li>
+            <li><a href="">Organic Fruits & Vegetables</a></li>
+            <li><a href="">Exotic Fruits</a></li>
+            <li><a href="">Canned & Frozen</a></li>
+            <li><a href="">Cold - Pressed Fresh Juices</a></li>
+            <li>
+                <h4><a href="">DELICATESSEN & CHEESE</a></h4>
+            </li>
+            <li><a href="">Fresh Snacks & Party Food</a></li>
+            <li><a href="">Antipasti</a></li>
+            <li><a href="">Gourmet Cheese Shop</a></li>
+            <li><a href="">Cold Cuts & Sausages</a></li>
+            <li><a href="">Cheese Spread, Slices & Others</a></li>
+        </ul>
+    </div>
+    <div>
+        <ul id="Adisecrow1">
+            <li>
+                <h4><a href="">INTERNATIONAL CUISINE</a></h4>
+            </li>
+            <li><a href="">Italian & Continental</a></li>
+            <li><a href="">Middle-Eastern</a></li>
+            <li><a href="">Condiments</a></li>
+            <li><a href="">Flours, Grains & More</a></li>
+            <li><a href="">Mexican</a></li>
+            <li><a href="">Salad Dressing</a></li>
+            <li><a href="">Vinegars & Mustards</a></li>
+            <li><a href="">Salt, Sugar & Substitutes</a></li>
+            <li><a href="">Pan-Asian</a></li>
+            <li><a href="">Baking Ingredients</a></li>
+            <li><a href="">Oils</a></li>
+            <li><a href="">Soups</a></li>
+            <li>
+                <h4><a href="">HEALTH</a></h4>
+            </li>
+            <li><a href="">Weight Management Foods</a></li>
+            <li><a href="">Heart - Healthy Foods</a></li>
+            <li><a href="">Wellness & Immunity Boosters</a></li>
+            <li><a href="">Diabetic - Friendly Foods</a></li>
+            <li><a href="">Problem Solution Foods</a></li>
+            <li><a href="">Organic</a></li>
+         </ul>
+    </div>
+    <div>
+        <ul id="Adisecrow1">
+            <li>
+                <h4><a href="">CONFECTIONARY & PATISSERIE</a></h4>
+            </li>
+            <li><a href="">Chocolates</a></li>
+            <li><a href="">Dessert Mixes, Sauces & Toppings</a></li>
+            <li><a href="">Ice Creams & Desserts</a></li>
+            <li><a href="">Confectionery</a></li>
+            <li><a href="">Baking Ingredients</a></li>
+            <li><a href="">Patisserie</a></li>
+            <li>
+                <h4><a href="">MEATS, SEAFOOD AND EGGS</a></h4>
+            </li>
+            <li><a href="">Fresh Meats & Seafood</a></li>
+            <li><a href="">Cold Cuts & Sausages</a></li>
+            <li><a href="">Packaged Sea food & Meats</a></li>
+            <li><a href="">Eggs</a></li>
+            <li><a href="">Frozen</a></li>
+        </ul>
+    </div>
+    <div>
+        <ul id="Adisecrow1">
+            <li>
+                <h4><a href=""> BREAKFAST & DAIRY</a></h4>
+            </li>
+            <li><a href="">Cereals, Bars & Others</a></li>
+            <li><a href="">Jams, Preserves & Conserves</a></li>
+            <li><a href="">Honey</a></li>
+            <li><a href="">Indian Breakfast Favourites</a></li>
+            <li><a href="">Spreads</a></li>
+            <li><a href="">Milk, Yogurt & Other Dairy Drinks</a></li>
+            <li><a href="">Butters & Margarines</a></li>
+            <li><a href="">Bakery</a></li>
+            <li>
+                <h4><a href="">INSTANT MEAL & AIDS</a></h4>
+            </li>
+            <li><a href="">Ready to Cook</a></li>
+            <li><a href="">Ready to Eat</a></li>
+            <li><a href="">Accessories</a></li>
+            <li><a href="">Books</a></li>
+        </ul>
+    </div>
+    <div>
+        <ul id="Adisecrow1">
+            <li>
+                <h4><a href=""> SNACKS & BEVERAGES</a></h4>
+            </li>
+            <li><a href="">Cookies & Crackers</a></li>
+            <li><a href="">Frozen Snacks</a></li>
+            <li><a href="">Antipasti</a></li>
+            <li><a href="">Dry Snacks</a></li>
+            <li><a href="">Fresh Snacks & Party Food</a></li>
+            <li><a href="">Relishes & Pickles</a></li>
+            <li><a href="">Carbonated Drinks</a></li>
+            <li><a href="">Teas</a></li>
+            <li><a href="">Concentrate</a></li>
+            <li><a href="">Energy Drinks & Milk Additives</a></li>
+            <li><a href="">Juice</a></li>
+            <li><a href="">Coffee</a></li>
+            <li><a href="">Dairy Drinks</a></li>
+            <li><a href="">Water</a></li>
+            <li>
+                <h4><a href="">INDIAN GROCERY</a></h4>
+            </li>
+            <li><a href="">Flours, Grains & More</a></li>
+            <li><a href="">Cooking Pastes & Sauces</a></li>
+            <li><a href="">Cooking Spices & Powders</a></li>
+            <li><a href="">Salt, Sugar & Substitutes</a></li>
+            <li><a href="">Oils</a></li>
+    </div>
+</div>
+
+<div id="Adifooter4">
+   <ul id="Adisecrow2">
+        <li><p>© 2020 Natures Basket Limited.  All rights reserved.</p></li>
+        <li><span><i class="fa-solid fa-pipe"></i></span></li>
+        <li><a href="https://www.naturesbasket.co.in/HTML/PrivacyPolicy.aspx">Privacy Policy</a></li>
+        <li><span><i class="fa-solid fa-pipe"></i></span></li>
+        <li><a href="https://www.naturesbasket.co.in/HTML/TermOfUse.aspx">Term Of Use</a></li>
+        <li><span><i class="fa-solid fa-pipe"></i></span></li>
+        <li><p>Home Delivery Number - 8880077745</p></li>
+        <li><span><i class="fa-solid fa-pipe"></i></span></li>
+        <li><a href="https://www.naturesbasket.co.in/HTML/SiteMap.aspx">Site Map</a></li>
+        <li><span><i class="fa-solid fa-pipe"></i></span></li>
+        <li><a href="">Mobile Site</a></li>
+        <li><span><i class="fa-solid fa-pipe"></i></span></li>
+        <li><p>FSSAI License Number - 11519002000496 </p></li>
+   </ul>
+</div>
+    
+    `
+}
+
+export {footer}
