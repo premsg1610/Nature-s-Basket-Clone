1537ce85b4e2be73fa185eb90dc61a5f8248f5bd
premsg1610 <premsgupta1997@gmail.com>
2022-05-08 22:01:53 +0530

    finally not done

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,3 +1,13 @@
+
+import {footer} from "../components/footer.js";
+let n =document.getElementById("as_footer");
+n.innerHTML=footer();
+
+import header from "../components/header.js"
+document.getElementById("header").innerHTML=header()
+
+
+
 var input=document.getElementById("btn");
 input.addEventListener("keypress", function(event){
   if(event.key=="Enter"){
